372a9e39d080c7a4fec67a841e710825650d9f77
Nochiis <john.nochii@gmail.com>
2025-06-21 11:59:45 -0500

    codigo con ya el número de palabras contadas

--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 def get_book_text():
     with open("books/frankenstein.txt", "r", encoding="utf-8") as f:
-        print (f.read())
-
+        return f.read()
+    
 def main():
     libro = get_book_text()
+    wordss()   
+    
+def wordss():
+    contador = 0
+    librosin = get_book_text()
+    words = librosin.split()
+    for i in words:
+        contador += 1 
 
+    num_words = f"{contador} words found in the document"
+    
+    print(num_words)
+    
 main()
